91c996c91a02f5895d16b9e6799a386051a0e49b
Andrin <andrinrenggli@outlook.com>
2025-03-11 11:47:43 +0100

    wrote controller. commited not working code

--- src/main/java/ch/bbw/tasks/TaskController.java
+++ src/main/java/ch/bbw/tasks/TaskController.java
@@ -1,4 +1,32 @@
 package ch.bbw.tasks;
 
+import ch.bbw.tasks.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/tasks")
+@CrossOrigin("*")
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskService.getTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable long id) {
+        return taskService.getTaskById(id)
+                .orElseThrow(() -> new RuntimeException("Task mit ID " + id + " nicht gefunden"));
+    }
+
+    @PostMapping
+    public Task saveTask(@RequestBody Task task) {
+        return taskService.saveTask(task);
+    }
+
 }
